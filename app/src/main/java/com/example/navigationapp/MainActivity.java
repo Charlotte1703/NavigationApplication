552fdbc37d039ebfaaee0e3a5d30010a27415b78
fryingpantheist <lottie1703@yahoo.com>
2019-04-11 17:23:55 +0100

    Snackbar created
    Added a Snackbar to the floating action button as a test

--- app/src/main/java/com/example/navigationapp/MainActivity.java
+++ app/src/main/java/com/example/navigationapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.navigationapp;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton navIcon = findViewById(R.id.navIconButton);
+        navIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Here's a Snackbar", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/navigationapp/NavIconButton.java
@@ -0,0 +1,11 @@
+package com.example.navigationapp;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+
+
+
+public class NavIconButton {
+
+}
